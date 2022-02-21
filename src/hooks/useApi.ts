b6796c76d6813f41a6d4e7f32f581e2d2ea5c161
LuizFylip <54417643+fymorGod@users.noreply.github.com>
2022-02-21 10:22:11 -0300

    atualização v1.0.1

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -6,28 +6,16 @@ const api = axios.create({
 
 export const useApi = () => ({
     validateToken: async (token: string) => {
-        return {
-            user: {
-                id: 3, name: 'fylip', email: 'fylip@gmail.com'
-            }
-        }
-
         const response = await api.post('/validate', { token });
         return response.data;
     },
     signin: async (email: string, password: string) => {
-        return {
-            user: {
-                id: 3, name: 'fylip', email: 'fylip@gmail.com'
-            },
-            token: '123456789'
-        }
+
         const response = await api.post('/signin', {email, password });
         return response.data;
 
     },
     logout: async () => {
-        return {statu: true}
         const response = await api.post('/logout');
         return response.data;
     }
